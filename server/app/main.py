0997d04b94497574fa7899dc348a58b7311be0f1
Daniel Jordan <danieljordandev@gmail.com>
2020-11-29 17:25:12 -0500

    troubleshooting requirements

--- server/app/main.py
+++ server/app/main.py
@@ -1,12 +1,31 @@
 import pdb
 from fastapi import FastAPI
-from pydantic import Json
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from starlette.requests import Request
+from typing import Dict, Any
 from .scripts.generator import translate
+
+
+class BasicRequest(BaseModel):
+    data: Dict[Any, Any]
+
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
 
-@app.get("/")
-async def convert(data: Json):
-    return {"model": translate(data)}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+    allow_headers=["*"],
+)
+
+@app.post("/")
+async def convert(basicRequest: BasicRequest):
+    # return {"model": translate(request.data)}
+    return {"model": "lol"}
