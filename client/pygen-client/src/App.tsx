263836aa53ec3e8c30306c5ae2475cec67806a21
brokenloop <brokenloop@users.noreply.github.com>
2020-11-29 17:25:12 -0500

    troubleshooting requirements

--- client/pygen-client/src/App.tsx
+++ client/pygen-client/src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import AceEditor from "react-ace";
 
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-monokai";
 
+const apiUrl = "http://localhost:8000";
+
+type RequestBody = {
+  data: string;
+};
+
 function onChange(newValue: string) {
+  const requestBody: RequestBody = { data: newValue };
   console.log(newValue);
+  const url = new URL(apiUrl);
+  // url.search = new URLSearchParams(requestBody).toString();
+  const blah = { data: { hello: 5 } };
+  const s = JSON.stringify(blah);
+  console.log(s);
+  const opts = {
+    method: "POST",
+    body: s,
+    // body: JSON.stringify(requestBody),
+  };
+
+  console.log(url.toString());
+  console.log(opts);
+  fetch(url.toString(), opts).then((x) => console.log(x));
 }
 
 function App() {
   return (
     <div className="App">
-        <AceEditor
-          mode="json"
-          theme="monokai"
-          onChange={onChange}
-          name="left-editor"
-          editorProps={{ $blockScrolling: true }}
-        />
+      <AceEditor
+        mode="json"
+        theme="monokai"
+        onChange={onChange}
+        name="left-editor"
+        editorProps={{ $blockScrolling: true }}
+      />
     </div>
   );
 }
