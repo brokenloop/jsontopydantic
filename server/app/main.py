e9f260a0a1f6903728f376589a11366601372bf3
brokenloop <brokenloop@users.noreply.github.com>
2020-11-29 20:20:17 -0500

    adding prod server

--- server/app/main.py
+++ server/app/main.py
@@ -1,19 +1,11 @@
-import pdb
-import logging
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Json
 from starlette.requests import Request
 from typing import Dict, Any
+from mangum import Mangum
+
 from .scripts.generator import translate
-
-
-logger = logging.getLogger(__name__)
-
-
-class BasicRequest(BaseModel):
-    data: Json
 
 
 app = FastAPI()
@@ -26,16 +18,18 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST", "GET"],
+    allow_methods=["POST"],
     allow_headers=["*"],
 )
 
 
-@app.get("/testget")
-async def testget():
-    return {"model": "success"}
+class BasicRequest(BaseModel):
+    data: Json
 
 
 @app.post("/")
 async def convert(basicRequest: BasicRequest):
     return {"model": translate(basicRequest.data)}
+
+
+handler = Mangum(app)
