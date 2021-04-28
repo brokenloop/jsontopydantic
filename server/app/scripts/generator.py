2b7b00869c8b9bb190d175fa76ff98b6fa4adb75
Matthieu Desprez <matthieu.desprez@gmail.com>
2021-04-28 15:20:44 +0200

    add optional and snake_cased options features

--- server/app/scripts/generator.py
+++ server/app/scripts/generator.py
@@ -1,34 +1,33 @@
 import json
-from pydantic import Json
-from typing import Dict, Any
-from genson import SchemaBuilder
-from datamodel_code_generator.parser.jsonschema import JsonSchemaParser
+from typing import Dict
 from datamodel_code_generator.model.pydantic import (
     BaseModel,
     CustomRootType,
     DataModelField,
 )
+from datamodel_code_generator.parser.jsonschema import JsonSchemaParser
+from genson import SchemaBuilder
+
 
 def translate(
-    input_text: Dict,
+        input_text: Dict, all_optional: bool, snake_case_field: bool
 ) -> str:
     builder = SchemaBuilder()
     builder.add_object(input_text)
-    schema = json.dumps(builder.to_schema())
+    schema = builder.to_schema()
+    if all_optional:
+        schema["required"] = []
     parser = JsonSchemaParser(
         BaseModel,
         CustomRootType,
         DataModelField,
-        text=schema,
+        text=json.dumps(schema),
+        snake_case_field=snake_case_field
     )
-
     return parser.parse()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     data = {"tester": 3}
-    result = translate(data)
+    result = translate(input_text=data, all_optional=True, snake_case_field=True)
     print(result)
-
-
-    
